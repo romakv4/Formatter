327769e7a2f88afa62992e0a773fedddfc6c0ff2
romakv4 <romakv4@gmail.com>
2016-10-28 00:15:49 +0600

    remove target/

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Main class.
+ */
+final class Main {
+    /**
+     * private constructor to eliminate errors.
+     */
+    private Main() { }
+    /**
+     *
+     * @param args is unused.
+     * This is a main method. It is works.
+     */
+    public static void main(final String[] args) throws IOException {
+    com.company.Reader read = new com.company.Reader();
+        FileWriter fw = new FileWriter("formattedText");
+        fw.write(Format.format(read.read()));
+        fw.flush();
+        //System.out.print(Format.format(read.read()));
+    }
+}
